431f6919d3a08db26fc8b48dcf2124311cd86b1a
qoxogus <a01034367055@gmail.com>
2021-05-13 17:18:26 +0900

    [기본문법과 쿼리 API]

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -2,10 +2,8 @@ package jpql;
 
 import jpql.entity.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -24,7 +22,19 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            em.createQuery("select m from Member m", Member.class);
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class) //TypedQuery 타입정보를 받을 수 있을 때(반환 타입이 명확할 때 사용)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result.getUsername());
+
+
+//            List<Member> resultList = query.getResultList(); //결과가 하나 이상이 일 대 리스트 반환 (결과가 없다면 빈 리스트를 반환한다)
+//
+//            Member result = query.getSingleResult(); // 결과가 정확히 하나, 단일 객체 반환 (결과가 없거나(NoResult) 둘 이상이면(NonUniqueResult) Exception)
+//            System.out.println("result = " + result);
+
+//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//            Query query3 = em.createQuery("select m.username, m.age from Member m"); //Query 반환타입이 명확하지 않을 때 사용
 
             tx.commit(); //트랜젝션 커밋시점에 쿼리가 나가게 된다
         } catch (Exception e) {
