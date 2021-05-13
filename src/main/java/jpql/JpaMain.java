958f9a3c83a1103d52bd468337bda9977c39d52d
qoxogus <a01034367055@gmail.com>
2021-05-13 17:46:41 +0900

    [프로젝션(SELECT)]

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import jpql.dto.MemberDTO;
+import jpql.entity.Address;
 import jpql.entity.Member;
 
 import javax.persistence.*;
@@ -22,11 +24,19 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class) //TypedQuery 타입정보를 받을 수 있을 때(반환 타입이 명확할 때 사용)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
+//            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class) //TypedQuery 타입정보를 받을 수 있을 때(반환 타입이 명확할 때 사용)
+//                    .setParameter("username", "member1")
+//                    .getSingleResult();
+//            System.out.println("result = " + result.getUsername());
 
+            em.flush();
+            em.clear();
+
+//            List<Member> result = em.createQuery("select m from Member m", Member.class) //select되는 대상이 많더라도 다 영속성 컨텍스트에 반영된다
+//                    .getResultList();
+//
+//            Member findMember = result.get(0);
+//            findMember.setAge(20); //영속성 컨텍스트에 관리되기 때문에 age가 정상적으로 20으로 바뀌는걸 확인할 수 있다
 
 //            List<Member> resultList = query.getResultList(); //결과가 하나 이상이 일 대 리스트 반환 (결과가 없다면 빈 리스트를 반환한다)
 //
@@ -35,6 +45,25 @@ public class JpaMain {
 
 //            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
 //            Query query3 = em.createQuery("select m.username, m.age from Member m"); //Query 반환타입이 명확하지 않을 때 사용
+
+//            em.createQuery("select o.address from Order o", Address.class) //임베디드타입 프로젝션
+//                    .getResultList();
+
+//            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m") //distinct : 중복제거  스칼라타입 프로젝션
+//                    .getResultList();
+
+//            Object o = resultList.get(0); List<Object[]>를 쓰면 2줄 생략 가능
+//           Object[] result = (Object[]) o;
+//            Object[] result = resultList.get(0);
+//            System.out.println("username = " + result[0]);
+//            System.out.println("age = " + result[1]);
+
+            List<MemberDTO> result = em.createQuery("select new jpql.dto.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit(); //트랜젝션 커밋시점에 쿼리가 나가게 된다
         } catch (Exception e) {
